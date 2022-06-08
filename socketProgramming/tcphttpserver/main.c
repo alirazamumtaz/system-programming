ecbb8562e5eda38ce33f4b8c8cfe83670135921f
Ali Raza <elirazamumtaz@gmail.com>
2022-06-08 19:07:21 +0500

    initial tcp server v0 is added

---
+++ socketProgramming/tcphttpserver/main.c
@@ -0,0 +1,42 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BACKLOG 10
+
+int main(){
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    int data_socket;
+
+    struct sockaddr_in server_address, client_address;
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(54154);
+    inet_aton("192.168.64.4",&server_address.sin_addr);
+    memset(&(server_address.sin_zero), '\0', sizeof(server_address));
+    bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
+
+    listen(server_socket,BACKLOG);
+    while (1){
+        fprintf(stderr,"\nServer waiting for client connection...");
+
+        int client_addr_length = sizeof(client_address);
+        data_socket = accept(server_socket,(struct sockaddr_in*)&client_address,client_addr_length);
+        fprintf(stderr,"\n*********  CONNECTION ESTABLISHED   **********\n");
+        
+        char buf[100];
+        int rv;
+        while (rv = read(data_socket, buf, sizeof(buf)))
+            write(1,buf,rv);
+        close(data_socket);
+    }
+    
+
+    return 0;
+}
